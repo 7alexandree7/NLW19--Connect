9761861c5c85fa5a0f630087208f26e5976fac6c
alexandre <alexandre020602@gmail.com>
2026-02-16 16:39:29 -0300

    config cors + docs swagger + link router + validation

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,31 @@
 import fastify from "fastify";
+import { fastifyCors } from '@fastify/cors'
+import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTransform } from "fastify-type-provider-zod"
+import { fastifySwagger } from "@fastify/swagger"
+import { fastifySwaggerUi } from "@fastify/swagger-ui"
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, { origin: true })
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: "Fastify API",
+            description: "API documentation",
+            version: "0.1.0"
+        }
+    },
+    transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUi, { routePrefix: "/docs" })
+app.register(subscribeToEventRoute)
+
 
 
 app.listen({ port: 3333 }).then(() => {
